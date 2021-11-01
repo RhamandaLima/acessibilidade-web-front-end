f297c448dc80f752db8af952869e85bdae870a2a
Rhamanda Lima <cathyannarhamanda@gmail.com>
2021-10-31 21:34:48 -0300

    Add sugestao de email melhorada

--- js/mailcheck-config.js
+++ js/mailcheck-config.js
@@ -1,22 +1,24 @@
-var campoEmail = document.querySelector('#email');
-var sugestao = document.querySelector('#campoEmail');
+var campoEmail = document.querySelector('#email')
+var sugestao = document.querySelector('#sugestao')
 
-var domains = ['gmail.com', 'uol.com', 'outlook.com', 'alura.com.br'];
+var domains = ['gmail.com', 'aol.com'];
 var secondLevelDomains = ['hotmail']
-var topLevelDomains = ["com", "net", "org", "br"];
+var topLevelDomains = ["com", "net", "org"];
 
-var superStringDistance = function (string1, string2) {
-    // a string distance algorithm of your choosing
-}
 
-document.querySelector('#email').addEventListener('blur', function () {
-    Mailcheck.run({
-        email: campoEmail.value,
-        domains: domains,                       // optional
-        topLevelDomains: topLevelDomains,       // optional
-        secondLevelDomains: secondLevelDomains, // optional
-        suggested: function (suggestion) {
-           console.log(suggestion.full)
-        }
-    });
+campoEmail.addEventListener('blur', function() {
+  Mailcheck.run({
+    email: campoEmail.value,
+    domains: domains,                       // optional
+    topLevelDomains: topLevelDomains,       // optional
+    secondLevelDomains: secondLevelDomains, // optional
+    suggested: function(suggestion) {
+      sugestao.style.display = 'inline-block';
+      sugestao.textContent = 'Você quis dizer: ' + suggestion.full + ' ?';
+      sugestao.parentNode.classList.add('contatoCampo--erro');
+      campoEmail.classList.add('contatoCampo--validouErro');
+      sugestao.setAttribute('tabindex', '0');
+      sugestao.setAttribute('role', 'alert');
+    }
+  });
 });
